8861add5400cc1f626b6e6511c4541afb0784956
Karthikeyan1508 <karthikeyan.36947@gmail.com>
2024-12-16 21:53:21 +0530

    fixed parse :((

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 const session = require("express-session");
 const flash = require("connect-flash");
+require('dotenv').config();
+
 const PORT = process.env.PORT || 5000;
 // Initialize Firebase Admin with the service account key
 
-if (!process.env.FIREBASE_CREDENTIALS) {
-  console.error("FIREBASE_CREDENTIALS is not defined in the environment variables!");
-  process.exit(1); // Stop the app
-}
-const credentials = process.env.FIREBASE_CREDENTIALS.replace(/\\n/g, '\n');
-const serviceAccount = JSON.parse(credentials);
+// if (!process.env.FIREBASE_CREDENTIALS) {
+//   console.error("FIREBASE_CREDENTIALS is not defined in the environment variables!");
+//   process.exit(1); // Stop the app
+// }
+// const credentials = process.env.FIREBASE_CREDENTIALS;
+// const serviceAccount = JSON.parse('');
 
 
-
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix newline formatting
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 // const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 
