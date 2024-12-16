d7881e65691004ccaa69ae832a47df57d5d91bff
Karthikeyan1508 <karthikeyan.36947@gmail.com>
2024-12-16 06:56:13 +0530

    check yaml

--- app.js
+++ app.js
@@ -202,6 +202,6 @@ app.get('/*', (req, res) => {
 });
 
 // Start the server
-app.listen(8000, () => {
-    console.log('Server is running on http://localhost:8000');
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
 });
